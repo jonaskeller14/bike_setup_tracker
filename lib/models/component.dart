42cd55ac1d3a257f743836e8c061df61c4f01d60
jonaskeller14 <jonaskeller14@googlemail.com>
2025-11-12 23:39:17 +0100

    Load adjustments from json

--- lib/models/component.dart
+++ lib/models/component.dart
@@ -12,6 +12,12 @@ class Component {
   Map<String, dynamic> toJson() => {'name': name, 'adjustments': adjustments.map((a) => a.toJson()).toList()};
 
   factory Component.fromJson(Map<String, dynamic> json) {
-    return Component(name: json['name']);
+    return Component(
+      name: json['name'],
+      adjustments: (json['adjustments'] as List<dynamic>?)
+              ?.map((a) => Adjustment.fromJson(a as Map<String, dynamic>))
+              .toList() ??
+          [],
+    );
   }
 }

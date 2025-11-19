c3387f894e84963fe26a1bc6ee80fe65bf0f5c76
jonaskeller14 <jonaskeller14@googlemail.com>
2025-11-19 09:11:08 +0100

    Add show more/less buttons for component/setting lists

--- lib/widgets/component_list.dart
+++ lib/widgets/component_list.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import '../models/component.dart';
 import '../widgets/adjustment_display_list.dart';
 
-class ComponentList extends StatelessWidget {
+class ComponentList extends StatefulWidget {
   final List<Component> components;
   final void Function(Component component) editComponent;
   final void Function(Component component) removeComponent;
@@ -15,82 +15,113 @@ class ComponentList extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      itemCount: components.length,
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      itemBuilder: (context, index) {
-        final component = components[index];
-        final componentAdjustmentValues = Map.fromEntries(
-          (component.currentSetting?.adjustmentValues ?? {})
-              .entries
-              .where((e) => component.adjustments.contains(e.key)),
-        );
+  State<ComponentList> createState() => _ComponentListState();
+}
 
-        return Card(
-          margin: const EdgeInsets.symmetric(vertical: 4.0),
-          child: ListTile(
-            leading: const Icon(Icons.casino),
-            contentPadding: const EdgeInsets.symmetric(
-              horizontal: 16,
-              vertical: 8,
-            ),
-            title: Text(
-              component.name,
-              style: const TextStyle(fontWeight: FontWeight.bold),
-            ),
-            subtitle: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Text('${component.adjustments.length} adjustments'),
+class _ComponentListState extends State<ComponentList> {
+  bool _expanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final visibleCount = _expanded
+        ? widget.components.length
+        : widget.components.length.clamp(0, 3);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ListView.builder(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: visibleCount,
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          itemBuilder: (context, index) {
+            final component = widget.components[index];
+            final componentAdjustmentValues = Map.fromEntries(
+              (component.currentSetting?.adjustmentValues ?? {})
+                  .entries
+                  .where((e) => component.adjustments.contains(e.key)),
+            );
+
+            return Card(
+              margin: const EdgeInsets.symmetric(vertical: 4.0),
+              child: ListTile(
+                leading: const Icon(Icons.casino),
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 8,
                 ),
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Text("Bike: ${component.bike.name}", overflow: TextOverflow.ellipsis),
+                title: Text(
+                  component.name,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
                 ),
-                AdjustmentDisplayList(
-                  adjustmentValues: componentAdjustmentValues,
+                subtitle: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Text('${component.adjustments.length} adjustments'),
+                    ),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Text("Bike: ${component.bike.name}", overflow: TextOverflow.ellipsis),
+                    ),
+                    AdjustmentDisplayList(
+                      adjustmentValues: componentAdjustmentValues,
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            trailing: PopupMenuButton<String>(
-              onSelected: (value) {
-                if (value == 'edit') {
-                  editComponent(component);
-                } else if (value == 'remove') {
-                  removeComponent(component);
-                }
+                trailing: PopupMenuButton<String>(
+                  onSelected: (value) {
+                    if (value == 'edit') {
+                      widget.editComponent(component);
+                    } else if (value == 'remove') {
+                      widget.removeComponent(component);
+                    }
+                  },
+                  itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+                    const PopupMenuItem<String>(
+                      value: 'edit',
+                      child: Row(
+                        children: [
+                          Icon(Icons.edit, size: 20),
+                          SizedBox(width: 10),
+                          Text('Edit'),
+                        ],
+                      ),
+                    ),
+                    const PopupMenuItem<String>(
+                      value: 'remove',
+                      child: Row(
+                        children: [
+                          Icon(Icons.delete, size: 20),
+                          SizedBox(width: 10),
+                          Text('Remove'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+
+        if (widget.components.length > 3)
+          Center(
+            child: TextButton.icon(
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
               },
-              itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-                const PopupMenuItem<String>(
-                  value: 'edit',
-                  child: Row(
-                    children: [
-                      Icon(Icons.edit, size: 20),
-                      SizedBox(width: 10),
-                      Text('Edit'),
-                    ],
-                  ),
-                ),
-                const PopupMenuItem<String>(
-                  value: 'remove',
-                  child: Row(
-                    children: [
-                      Icon(Icons.delete, size: 20),
-                      SizedBox(width: 10),
-                      Text('Remove'),
-                    ],
-                  ),
-                ),
-              ],
+              icon: Icon(
+                _expanded ? Icons.expand_less : Icons.expand_more,
+              ),
+              label: Text(_expanded ? "Show less" : "Show more"),
             ),
           ),
-        );
-      },
+      ],
     );
   }
 }
--- lib/widgets/setting_list.dart
+++ lib/widgets/setting_list.dart
@@ -3,7 +3,7 @@ import 'package:intl/intl.dart';
 import '../models/setting.dart';
 import '../widgets/adjustment_display_list.dart';
 
-class SettingList extends StatelessWidget {
+class SettingList extends StatefulWidget {
   final List<Setting> settings;
   final void Function(Setting setting) editSetting;
   final void Function(Setting setting) removeSetting;
@@ -16,104 +16,136 @@ class SettingList extends StatelessWidget {
   });
 
   @override
+  State<SettingList> createState() => _SettingListState();
+}
+
+class _SettingListState extends State<SettingList> {
+  bool _expanded = false;
+
+  @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      itemCount: settings.length,
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      itemBuilder: (context, index) {
-        final setting = settings[settings.length - 1 - index];
-        return Card(
-          margin: const EdgeInsets.symmetric(vertical: 4.0),
-          child: ListTile(
-            contentPadding: const EdgeInsets.symmetric(
-              horizontal: 16,
-              vertical: 8,
-            ),
-            title: Text(
-              setting.name,
-              style: const TextStyle(fontWeight: FontWeight.bold),
-            ),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  DateFormat('yyyy-MM-dd HH:mm').format(setting.datetime),
-                  style: TextStyle(color: Colors.grey.shade600, fontSize: 13),
-                  overflow: TextOverflow.ellipsis,
+    final visibleCount = _expanded
+        ? widget.settings.length
+        : widget.settings.length.clamp(0, 3);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ListView.builder(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: visibleCount,
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          itemBuilder: (context, index) {
+            final setting = widget.settings[widget.settings.length - 1 - index];
+
+            return Card(
+              margin: const EdgeInsets.symmetric(vertical: 4.0),
+              child: ListTile(
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                title: Text(
+                  setting.name,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
                 ),
-                if (setting.place != null) ... [
-                  Text(
-                    "${setting.place?.thoroughfare} ${setting.place?.subThoroughfare}, ${setting.place?.locality}, ${setting.place?.country}",
-                    style: TextStyle(color: Colors.grey.shade600, fontSize: 13),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ],
-                if (setting.position != null) ... [
-                  Text(
-                    "↑ Altitude: ${setting.position!.altitude!.round()} m",
-                    style: TextStyle(color: Colors.grey.shade600, fontSize: 13),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ],
-                if (setting.temperature != null) ...[
-                  Text(
-                    "${setting.temperature!.toStringAsFixed(1)} °C",
-                    style: TextStyle(color: Colors.grey.shade600, fontSize: 13),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ],
-                if (setting.notes != null && setting.notes!.isNotEmpty) ...[
-                  const SizedBox(height: 6),
-                  Text(
-                    setting.notes!,
-                    style: TextStyle(color: Colors.grey.shade700, fontSize: 13),
-                  ),
-                ],
-                const SizedBox(height: 6.0),
-                AdjustmentDisplayList(
-                  adjustmentValues: setting.adjustmentValues,
-                  previousAdjustmentValues:
-                      setting.previousSetting?.adjustmentValues,
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      DateFormat('yyyy-MM-dd HH:mm').format(setting.datetime),
+                      style: TextStyle(
+                        color: Colors.grey.shade600,
+                        fontSize: 13,
+                      ),
+                    ),
+                    if (setting.place != null)
+                      Text(
+                        "${setting.place?.thoroughfare} ${setting.place?.subThoroughfare}, "
+                        "${setting.place?.locality}, ${setting.place?.country}",
+                        style: TextStyle(
+                            color: Colors.grey.shade600, fontSize: 13),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    if (setting.position != null)
+                      Text(
+                        "↑ Altitude: ${setting.position!.altitude!.round()} m",
+                        style: TextStyle(
+                            color: Colors.grey.shade600, fontSize: 13),
+                      ),
+                    if (setting.temperature != null)
+                      Text(
+                        "${setting.temperature!.toStringAsFixed(1)} °C",
+                        style: TextStyle(
+                            color: Colors.grey.shade600, fontSize: 13),
+                      ),
+                    if (setting.notes != null && setting.notes!.isNotEmpty) ...[
+                      const SizedBox(height: 6),
+                      Text(
+                        setting.notes!,
+                        style: TextStyle(
+                            color: Colors.grey.shade700, fontSize: 13),
+                      ),
+                    ],
+                    const SizedBox(height: 6.0),
+                    AdjustmentDisplayList(
+                      adjustmentValues: setting.adjustmentValues,
+                      previousAdjustmentValues:
+                          setting.previousSetting?.adjustmentValues,
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            trailing: PopupMenuButton<String>(
-              onSelected: (value) {
-                if (value == 'edit') {
-                  editSetting(setting);
-                } else if (value == 'remove') {
-                  removeSetting(setting);
-                }
+                trailing: PopupMenuButton<String>(
+                  onSelected: (value) {
+                    if (value == 'edit') {
+                      widget.editSetting(setting);
+                    } else if (value == 'remove') {
+                      widget.removeSetting(setting);
+                    }
+                  },
+                  itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+                    const PopupMenuItem<String>(
+                      value: 'edit',
+                      child: Row(
+                        children: [
+                          Icon(Icons.edit, size: 20),
+                          SizedBox(width: 10),
+                          Text('Edit'),
+                        ],
+                      ),
+                    ),
+                    const PopupMenuItem<String>(
+                      value: 'remove',
+                      child: Row(
+                        children: [
+                          Icon(Icons.delete, size: 20),
+                          SizedBox(width: 10),
+                          Text('Remove'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+
+        if (widget.settings.length > 3)
+          Center(
+            child: TextButton.icon(
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
               },
-              itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-                const PopupMenuItem<String>(
-                  value: 'edit',
-                  child: Row(
-                    children: [
-                      Icon(Icons.edit, size: 20),
-                      SizedBox(width: 10),
-                      Text('Edit'),
-                    ],
-                  ),
-                ),
-                const PopupMenuItem<String>(
-                  value: 'remove',
-                  child: Row(
-                    children: [
-                      Icon(Icons.delete, size: 20),
-                      SizedBox(width: 10),
-                      Text('Remove'),
-                    ],
-                  ),
-                ),
-              ],
+              icon: Icon(
+                _expanded ? Icons.expand_less : Icons.expand_more,
+              ),
+              label: Text(_expanded ? "Show less" : "Show more"),
             ),
           ),
-        );
-      },
+      ],
     );
   }
 }

50971252f4c9a94791a6fb703a8ffe88c5530d7b
jonaskeller14 <jonaskeller14@googlemail.com>
2025-11-26 19:43:35 +0100

    Add discard changes dialog to ComponentPage

--- lib/pages/component_page.dart
+++ lib/pages/component_page.dart
@@ -7,6 +7,7 @@ import 'adjustment/numerical_adjustment_page.dart';
 import 'adjustment/step_adjustment_page.dart';
 import 'adjustment/categorical_adjustment_page.dart';
 import '../widgets/adjustment_edit_list.dart';
+import '../../widgets/dialogs/discard_changes.dart';
 
 class ComponentPage extends StatefulWidget {
   final Component? component;
@@ -20,8 +21,10 @@ class ComponentPage extends StatefulWidget {
 
 class _ComponentPageState extends State<ComponentPage> {
   final _formKey = GlobalKey<FormState>();
+  bool _formHasChanges = false;
   late TextEditingController _nameController;
   late List<Adjustment> adjustments;
+  late List<Adjustment> initialAdjustments;
   late Bike bike;
   late ComponentType? componentType;
 
@@ -29,13 +32,38 @@ class _ComponentPageState extends State<ComponentPage> {
   void initState() {
     super.initState();
     _nameController = TextEditingController(text: widget.component?.name);
+    _nameController.addListener(_changeListener);
     adjustments = widget.component?.adjustments ?? [];
+    initialAdjustments = List.from(adjustments);
     bike = widget.component?.bike ?? widget.bikes.first;
     componentType = widget.component?.componentType;
   }
 
+  void _changeListener() {
+    final nameHasChanges = _nameController.text.trim() != (widget.component?.name ?? '');
+    final bikeHasChanges = bike != (widget.component?.bike ?? widget.bikes.first);
+    final componentTypeHasChanges = componentType != widget.component?.componentType;
+    
+    bool adjustmentListHasChanges = initialAdjustments.length != adjustments.length;
+    if (!adjustmentListHasChanges) {
+      for (int index = 0; index < adjustments.length; index++) {
+        if (adjustments[index] != initialAdjustments[index]) {
+          adjustmentListHasChanges = true;
+          break;
+        }
+      }
+    }
+    final hasChanges = nameHasChanges || componentTypeHasChanges || bikeHasChanges || adjustmentListHasChanges;
+    if (_formHasChanges != hasChanges) {
+      setState(() {
+        _formHasChanges = hasChanges;
+      });
+    }
+  }
+
   @override
   void dispose() {
+    _nameController.removeListener(_changeListener);
     _nameController.dispose();
     super.dispose();
   }
@@ -50,6 +78,7 @@ class _ComponentPageState extends State<ComponentPage> {
         adjustments.add(adjustment);
       });
     }
+    _changeListener();
   }
 
   Future<void> _addNumericalAdjustment() async {
@@ -62,6 +91,7 @@ class _ComponentPageState extends State<ComponentPage> {
         adjustments.add(adjustment);
       });
     }
+    _changeListener();
   }
 
   Future<void> _addStepAdjustment() async {
@@ -74,6 +104,7 @@ class _ComponentPageState extends State<ComponentPage> {
         adjustments.add(adjustment);
       });
     }
+    _changeListener();
   }
 
   Future<void> _addCategoricalAdjustment() async {
@@ -86,6 +117,7 @@ class _ComponentPageState extends State<ComponentPage> {
         adjustments.add(adjustment);
       });
     }
+    _changeListener();
   }
 
   Future<void> _editAdjustment(Adjustment adjustment) async {
@@ -168,12 +200,14 @@ class _ComponentPageState extends State<ComponentPage> {
     setState(() {
       adjustments.remove(adjustment);
     });
+    _changeListener();
   }
 
   void _saveComponent() {
     if (!_formKey.currentState!.validate()) return;
     final name = _nameController.text.trim();
-
+    _formHasChanges = false;
+    if (!mounted) return;
     Navigator.pop(
       context,
       Component(
@@ -187,155 +221,175 @@ class _ComponentPageState extends State<ComponentPage> {
     );
   }
 
+  void _handlePopInvoked(bool didPop, dynamic result) async {
+    if (didPop) return;
+    if (!_formHasChanges) return;
+    final shouldDiscard = await showDiscardChangesDialog(context);
+    if (!mounted) return;
+    if (!shouldDiscard) return;
+    Navigator.of(context).pop(null);
+  }
+
+  String? _validateName(String? value) {
+    if (value == null || value.trim().isEmpty) return 'Name is required';
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: widget.component == null ? const Text('Add Component') : const Text('Edit Component'),
-        actions: [
-          IconButton(icon: const Icon(Icons.check), onPressed: _saveComponent),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Form(
-          key: _formKey,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                TextFormField(
-                  controller: _nameController,
-                  textInputAction: TextInputAction.next,
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  autofocus: widget.component == null,
-                  decoration: const InputDecoration(
-                    labelText: 'Component Name',
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter component name',
+    return PopScope( 
+      canPop: !_formHasChanges,
+      onPopInvokedWithResult: _handlePopInvoked,
+      child: Scaffold(
+        appBar: AppBar(
+          title: widget.component == null ? const Text('Add Component') : const Text('Edit Component'),
+          actions: [
+            IconButton(icon: const Icon(Icons.check), onPressed: _saveComponent),
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextFormField(
+                    controller: _nameController,
+                    textInputAction: TextInputAction.next,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    autofocus: widget.component == null,
+                    decoration: const InputDecoration(
+                      labelText: 'Component Name',
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter component name',
+                    ),
+                    validator: _validateName,
                   ),
-                  validator: (value) {
-                    if (value == null || value.trim().isEmpty) {
-                      return 'Component name cannot be empty';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 12),
-                DropdownButtonFormField<Bike>(
-                  initialValue: bike,
-                  isExpanded: true,
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  decoration: const InputDecoration(
-                    labelText: 'Bike',
-                    border: OutlineInputBorder(),
-                    hintText: "Choose a bike for this component",
+                  const SizedBox(height: 12),
+                  DropdownButtonFormField<Bike>(
+                    initialValue: bike,
+                    isExpanded: true,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    decoration: const InputDecoration(
+                      labelText: 'Bike',
+                      border: OutlineInputBorder(),
+                      hintText: "Choose a bike for this component",
+                    ),
+                    items: widget.bikes.map((b) {
+                      return DropdownMenuItem<Bike>(
+                        value: b,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            const Icon(Icons.pedal_bike),
+                            const SizedBox(width: 8),
+                            Expanded(child: Text(b.name, overflow: TextOverflow.ellipsis))
+                          ],
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: (Bike? newBike) {
+                      if (newBike == null) return;
+                      setState(() {
+                        bike = newBike;
+                      });
+                      _changeListener();
+                    },
                   ),
-                  items: widget.bikes.map((b) {
-                    return DropdownMenuItem<Bike>(
-                      value: b,
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          const Icon(Icons.pedal_bike),
-                          const SizedBox(width: 8),
-                          Expanded(child: Text(b.name, overflow: TextOverflow.ellipsis))
-                        ],
-                      ),
-                    );
-                  }).toList(),
-                  onChanged: (Bike? newBike) {
-                    if (newBike == null) return;
-                    setState(() {
-                      bike = newBike;
-                    });
-                  },
-                ),
-                const SizedBox(height: 12),
-                DropdownButtonFormField<ComponentType>(
-                  initialValue: componentType,
-                  isExpanded: true,
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  hint: const Text("Please select type"),
-                  decoration: const InputDecoration(
-                    labelText: 'Type',
-                    border: OutlineInputBorder(),
-                    hintText: "Choose a type for this component",
+                  const SizedBox(height: 12),
+                  DropdownButtonFormField<ComponentType>(
+                    initialValue: componentType,
+                    isExpanded: true,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    hint: const Text("Please select type"),
+                    decoration: const InputDecoration(
+                      labelText: 'Type',
+                      border: OutlineInputBorder(),
+                      hintText: "Choose a type for this component",
+                    ),
+                    items: ComponentType.values.map((componentType) {
+                      return DropdownMenuItem<ComponentType>(
+                        value: componentType,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Component.getIcon(componentType),
+                            const SizedBox(width: 8),
+                            Expanded(child: Text(componentType.value, overflow: TextOverflow.ellipsis)),
+                          ],
+                        ),
+                      );
+                    }).toList(),
+                    onChanged: (ComponentType? newComponentType) {
+                      if (newComponentType == null) return;
+                      setState(() {
+                        componentType = newComponentType;
+                      });
+                      _changeListener();
+                    },
+                    validator: (value) {
+                      if (value == null) {
+                        return 'Component type cannot be empty';
+                      }
+                      return null;
+                    },
                   ),
-                  items: ComponentType.values.map((componentType) {
-                    return DropdownMenuItem<ComponentType>(
-                      value: componentType,
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Component.getIcon(componentType),
-                          const SizedBox(width: 8),
-                          Expanded(child: Text(componentType.value, overflow: TextOverflow.ellipsis)),
-                        ],
+                  const SizedBox(height: 12),
+                  Wrap(
+                    spacing: 8.0,
+                    runSpacing: 4.0,
+                    children: [
+                      ActionChip(
+                        avatar: Icon(Icons.add),
+                        label: const Text('Add On/Off Adjustment'),
+                        onPressed: _addBooleanAdjustment,
                       ),
-                    );
-                  }).toList(),
-                  onChanged: (ComponentType? newComponentType) {
-                    if (newComponentType == null) return;
-                    setState(() {
-                      componentType = newComponentType;
-                    });
-                  },
-                  validator: (value) {
-                    if (value == null) {
-                      return 'Component type cannot be empty';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 12),
-                Wrap(
-                  spacing: 8.0,
-                  runSpacing: 4.0,
-                  children: [
-                    ActionChip(
-                      avatar: Icon(Icons.add),
-                      label: const Text('Add On/Off Adjustment'),
-                      onPressed: _addBooleanAdjustment,
-                    ),
-                    ActionChip(
-                      avatar: Icon(Icons.add),
-                      label: const Text('Add Categorical Adjustment'),
-                      onPressed: _addCategoricalAdjustment,
-                    ),
-                    ActionChip(
-                      avatar: Icon(Icons.add),
-                      label: const Text('Add Step Adjustment'),
-                      onPressed: _addStepAdjustment,
-                    ),
-                    ActionChip(
-                      avatar: Icon(Icons.add),
-                      label: const Text('Add Numerical Adjustment'),
-                      onPressed: _addNumericalAdjustment,
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 16),
-                adjustments.isNotEmpty 
-                    ? AdjustmentEditList(
-                        adjustments: adjustments,
-                        editAdjustment: _editAdjustment,
-                        removeAdjustment: removeAdjustment,
-                        onReorderAdjustments: (List<Adjustment> tmpAdjustments) => {adjustments = tmpAdjustments},
-                      ) 
-                    : Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 20),
-                        child: Center(
-                          child: Text(
-                            'No adjustments yet',
-                            style: TextStyle(color: Colors.grey.shade600),
-                          ),
-                        )
+                      ActionChip(
+                        avatar: Icon(Icons.add),
+                        label: const Text('Add Categorical Adjustment'),
+                        onPressed: _addCategoricalAdjustment,
                       ),
-              ],
+                      ActionChip(
+                        avatar: Icon(Icons.add),
+                        label: const Text('Add Step Adjustment'),
+                        onPressed: _addStepAdjustment,
+                      ),
+                      ActionChip(
+                        avatar: Icon(Icons.add),
+                        label: const Text('Add Numerical Adjustment'),
+                        onPressed: _addNumericalAdjustment,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  adjustments.isNotEmpty 
+                      ? AdjustmentEditList(
+                          adjustments: adjustments,
+                          editAdjustment: _editAdjustment,
+                          removeAdjustment: removeAdjustment,
+                          onReorderAdjustments: (List<Adjustment> tmpAdjustments) {
+                            setState(() {
+                              adjustments = tmpAdjustments;
+                            });
+                            _changeListener();
+                          },
+                        ) 
+                      : Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 20),
+                          child: Center(
+                            child: Text(
+                              'No adjustments yet',
+                              style: TextStyle(color: Colors.grey.shade600),
+                            ),
+                          )
+                        ),
+                ],
+              ),
             ),
           ),
         ),

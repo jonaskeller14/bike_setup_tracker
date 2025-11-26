c8f4c6e996c87de5defd93879ddeb3a0258672bf
jonaskeller14 <jonaskeller14@googlemail.com>
2025-11-26 15:51:35 +0100

    Added new for for SetAltitudeDialog

---
+++ lib/widgets/dialogs/set_altitude.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+Future<double?> showSetAltitudeDialog(BuildContext context, double? altitude) async {
+  return await showDialog<double?>(
+    context: context,
+    builder: (BuildContext context) {
+      final formKey = GlobalKey<FormState>();
+      final controller = TextEditingController(text: altitude?.toString());
+      return AlertDialog(
+        scrollable: true,
+        title: Text('Set Altitude'),
+        content: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  keyboardType: const TextInputType.numberWithOptions(decimal: true, signed: true),
+                  inputFormatters: [FilteringTextInputFormatter.allow(RegExp(r'^-?\d*\.?\d*$')),],
+                  controller: controller,
+                  autofocus: true,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    isDense: true,
+                    hintText: 'Altitude',
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                    suffixText: 'm',
+                    icon: Icon(Icons.arrow_upward),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Please enter a value';
+                    }
+                    final parsedValue = double.tryParse(value);
+                    if (parsedValue == null) return "Please enter valid number";
+                    return null;
+                  },
+                  onFieldSubmitted: (_) {
+                    if (!formKey.currentState!.validate()) return;
+                    Navigator.of(context).pop(double.parse(controller.text.trim()));
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {Navigator.of(context).pop(null);},
+            child: const Text("Cancel"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              if (!formKey.currentState!.validate()) return;
+              Navigator.of(context).pop(double.parse(controller.text.trim()));
+            },
+            child: const Text("Submit"),
+          ),
+        ],
+      );
+    }
+  );
+}

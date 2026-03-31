e767f30f8617303a78d79d3d620aab4ea688b852
jonaskeller14 <jonaskeller14@googlemail.com>
2026-03-31 10:02:38 +0200

    RatingDetailsPage: Added filter card and fixed wrong appbar title icon

--- lib/pages/details/rating_details_page.dart
+++ lib/pages/details/rating_details_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import '../../models/rating.dart';
+import '../../models/bike.dart';
 import '../../models/person.dart';
+import '../../models/component.dart';
 import '../../repositories/app_repository.dart';
 import '../../utils/rating_actions.dart';
 
@@ -12,6 +15,9 @@ class RatingDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final appRepository = context.watch<AppRepository>();
+    final bikes = appRepository.bikes;
+    final persons = appRepository.persons;
+    final components = appRepository.components;
 
     final rating = appRepository.ratings[ratingId];
     if (rating == null) return const SizedBox.shrink();
@@ -21,7 +27,7 @@ class RatingDetailsPage extends StatelessWidget {
         title: Row(
           spacing: 8,
           children: [
-            const Icon(Person.iconData),
+            const Icon(Rating.iconData),
             Expanded(
               child: Text(rating.name, overflow: TextOverflow.ellipsis),
             ),
@@ -49,36 +55,57 @@ class RatingDetailsPage extends StatelessWidget {
                     dense: true,
                   ),
                 ),
-              // Card.outlined(
-              //   margin: const EdgeInsets.symmetric(vertical: 4),
-              //   child: ListTile(
-              //     leading: const Icon(Icons.filter_alt_outlined),
-              //     title: Row(
-              //       spacing: 8,
-              //       children: [
-              //         switch (rating.filterType) {
-              //           FilterType.global => const Icon(Icons.circle_outlined),
-              //           FilterType.bike => const Icon(Bike.iconData),
-              //           FilterType.person => const Icon(Person.iconData),
-              //           FilterType.component => ,
-              //           FilterType.componentType => ,
-              //         },
-              //         Expanded(
-              //           child: Text(
-              //             //FIXME
-              //             "Apply everywhere", 
-              //             overflow: TextOverflow.ellipsis
-              //           ),
-              //         ),
-              //       ],
-              //     ),
-              //     dense: true,
-              //   ),
-              // ),
+              Card.outlined(
+                margin: const EdgeInsets.symmetric(vertical: 4),
+                child: ListTile(
+                  leading: const Icon(Icons.filter_alt_outlined),
+                  title: Row(
+                    spacing: 8,
+                    children: switch (rating.filterType) {
+                      FilterType.global => [
+                          const Icon(Icons.circle_outlined),
+                          const Expanded(child: Text("Apply everywhere", overflow: TextOverflow.ellipsis)),
+                        ],
+                      FilterType.bike => [
+                          Icon(Bike.iconData, color: bikes[rating.filter] == null ? Theme.of(context).colorScheme.error : null),
+                          Expanded(child: Text(
+                            bikes[rating.filter]?.name ?? "BIKE NOT FOUND", 
+                            overflow: TextOverflow.ellipsis,
+                            style: bikes[rating.filter] == null ? TextStyle(color: Theme.of(context).colorScheme.error) : null,
+                          )),
+                        ],
+                      FilterType.person => [
+                          Icon(Person.iconData, color: persons[rating.filter] == null ? Theme.of(context).colorScheme.error : null),
+                          Expanded(child: Text(
+                            persons[rating.filter]?.name ?? "PERSON NOT FOUND", 
+                            overflow: TextOverflow.ellipsis,
+                            style: persons[rating.filter] == null ? TextStyle(color: Theme.of(context).colorScheme.error) : null,
+                          )),
+                        ],
+                      FilterType.component => [
+                          Icon(
+                            components[rating.filter]?.componentType.getIconData() ?? Icons.error,
+                            color: components[rating.filter] == null ? Theme.of(context).colorScheme.error : null,
+                          ),
+                          Expanded(child: Text(
+                            components[rating.filter]?.name ?? "COMPONENT NOT FOUND", 
+                            overflow: TextOverflow.ellipsis,
+                            style: components[rating.filter] == null ? TextStyle(color: Theme.of(context).colorScheme.error) : null,
+                          )),
+                        ],
+                      FilterType.componentType => [
+                          Icon(ComponentType.fromString(rating.filter).getIconData()),
+                          Expanded(child: Text(ComponentType.fromString(rating.filter).label, overflow: TextOverflow.ellipsis)),
+                        ],
+                    },
+                  ),
+                  dense: true,
+                ),
+              ),
               //TODO: Table view setup rating adjustment vlaues (analogue to ComponetDetailsPage)
             ],
           ),
-        )
+        ),
       ),
     );
   }

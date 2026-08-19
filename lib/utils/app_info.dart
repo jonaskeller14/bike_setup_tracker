49f1f96c3fd8c690c8d3dcaf1be0d04f8f6457e2
jonaskeller14 <jonaskeller14@googlemail.com>
2026-08-19 19:48:49 +0200

    Release v1.3.11+35

--- lib/utils/app_info.dart
+++ lib/utils/app_info.dart
@@ -6,8 +6,8 @@
 class AppInfo {
   AppInfo._();
 
-  static const String appVersion = '1.3.10';
-  static const String buildNumber = '34';
+  static const String appVersion = '1.3.11';
+  static const String buildNumber = '35';
   static const String releaseDate = 'August 2026';
 
   static const String supportEmail = 'jonaskeller14.app+support@gmail.com';

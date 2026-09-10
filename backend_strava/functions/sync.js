e6a5e64d11e6d04727df89055d08d74c2fbf50f5
jonaskeller14 <jonaskeller14@googlemail.com>
2026-09-10 11:17:09 +0200

    refactor: replace hardcoded activity batch size with constant

--- backend_strava/functions/sync.js
+++ backend_strava/functions/sync.js
@@ -1,6 +1,7 @@
 const { onCall, HttpsError } = require("firebase-functions/v2/https");
 const { db, logger, admin } = require("./firebase");
 const {
+  ACTIVITY_BATCH_SIZE,
   getValidAccessToken,
   saveAthleteAndGear,
   saveActivityToBatch,
@@ -197,8 +198,8 @@ async function syncFullHistory(athleteId) {
 
         allActivitiesSaved++;
 
-        // When we have 500, write a batch doc
-        if (currentBatchActivities.length >= 500) {
+        // When we have a full batch, write a batch doc
+        if (currentBatchActivities.length >= ACTIVITY_BATCH_SIZE) {
           await writeBatchDoc(athleteId, currentBatchActivities, allActivitiesSaved);
           currentBatchActivities = [];
         }
@@ -239,7 +240,7 @@ async function syncFullHistory(athleteId) {
 }
 
 async function writeBatchDoc(athleteId, activitiesRaw, totalCountSoFar) {
-  const batchNum = Math.ceil(totalCountSoFar / 500);
+  const batchNum = Math.ceil(totalCountSoFar / ACTIVITY_BATCH_SIZE);
   const batchId = `batch_${String(batchNum).padStart(3, "0")}`;
 
   const activityIds = activitiesRaw.map((a) => a.id);

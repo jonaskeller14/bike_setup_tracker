bc794d16fbc1c4fc73fb462e6074ab4f075c0f0a
jonaskeller14 <jonaskeller14@googlemail.com>
2026-09-25 19:59:37 +0200

    feat(setup): add animated rotation for sort order icon in FilterChip

--- lib/widgets/chips/setup_list_sort.dart
+++ lib/widgets/chips/setup_list_sort.dart
@@ -12,9 +12,12 @@ class SetupListSort extends StatelessWidget {
     return FilterChip(
       materialTapTargetSize: MaterialTapTargetSize.shrinkWrap, // Removes the 48px constraint
       labelPadding: const EdgeInsets.symmetric(vertical: 2),
-      avatar: appRepository.stravaSortAscending 
-          ? const Icon(Icons.arrow_upward) 
-          : const Icon(Icons.arrow_downward),
+      avatar: AnimatedRotation(
+        turns: appRepository.stravaSortAscending ? 0 : 0.5,
+        duration: const Duration(milliseconds: 250),
+        curve: Curves.easeInOut,
+        child: const Icon(Icons.arrow_upward),
+      ),
       label: const SizedBox.shrink(), 
       onSelected: (bool value) => appRepository.setStravaSortOrder(value),
       selected: appRepository.stravaSortAscending,
